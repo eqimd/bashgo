13200d20161b0d31e2eec96420867fb8e56a527c
Daniil Lyubaev <lyubaevda@gmail.com>
2023-10-10 15:48:18 +0300

    External command done

--- internal/command/external/external_command.go
+++ internal/command/external/external_command.go
@@ -1,6 +1,10 @@
 package external
 
 import (
+	"errors"
+	"io"
+	"os/exec"
+
 	"github.com/eqimd/bashgo/internal/command"
 	"github.com/eqimd/bashgo/internal/data"
 )
@@ -10,8 +14,43 @@ type ExternalCommand struct {
 }
 
 func (cmd *ExternalCommand) Run(args []command.CommandArgument, input *data.Input) (*data.Output, error) {
-	// TODO
-	return nil, nil
+	strArgs := make([]string, 0, len(args))
+	for _, arg := range args {
+		strArgs = append(strArgs, string(arg))
+	}
+
+	outp := &data.Output{}
+
+	extCmd := exec.Command(cmd.executablePath, strArgs...)
+
+	if input != nil {
+		stdin, err := extCmd.StdinPipe()
+		if err != nil {
+			return nil, err
+		}
+
+		_, err = io.WriteString(stdin, input.Data)
+		if err != nil {
+			return nil, err
+		}
+
+		stdin.Close()
+	}
+
+	out, err := extCmd.CombinedOutput()
+
+	outp.Data = string(out)
+
+	if err != nil {
+		var exitErr *exec.ExitError
+		if errors.As(err, &exitErr) {
+			outp.ExitCode = exitErr.ExitCode()
+		} else {
+			return nil, err
+		}
+	}
+
+	return outp, nil
 }
 
 func NewExternalCommand(executablePath string) *ExternalCommand {
