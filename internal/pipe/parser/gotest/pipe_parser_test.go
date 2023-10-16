bae6346a75cfb8c7e4ea5fa5f5e56f4e468e0e34
DenisKorotchenko <dzd1024ozd@gmail.com>
2023-10-16 18:51:30 +0300

    Add environment variables substitution

--- internal/pipe/parser/gotest/pipe_parser_test.go
+++ internal/pipe/parser/gotest/pipe_parser_test.go
@@ -1,6 +1,7 @@
 package parser_test
 
 import (
+	"os"
 	"testing"
 
 	"github.com/eqimd/bashgo/internal/command/splitter"
@@ -21,5 +22,45 @@ func TestPipeParserSimple(t *testing.T) {
 	out, err := pipe.RunPipe(nil)
 
 	require.NoError(t, err, "unexpected error: %v")
+	require.Equal(t, text, out.Data)
+}
+
+// Тест проверяет, что парсер успешно выполняет подстановку одной переменной среды окружения
+func TestPipeParserWithEnvVariable(t *testing.T) {
+	text := "text"
+	err := os.Setenv("test", text)
+	require.NoError(t, err)
+
+	line := "echo $test"
+
+	splitter := splitter.NewCommandSplitterImpl()
+	parser := parser.NewPipeParserImpl(splitter)
+
+	pipe := parser.Parse(line)
+
+	out, err := pipe.RunPipe(nil)
+
+	require.NoError(t, err)
+	require.Equal(t, text, out.Data)
+}
+
+// Тест проверяет, что парсер успешно выполняет подстановку нескольких переменных среды окружения, в том числе в качестве имени команды
+func TestPipeParserWithMultipleEnvVariables(t *testing.T) {
+	text := "text"
+	err := os.Setenv("com", "echo")
+	require.NoError(t, err)
+	err = os.Setenv("test", text)
+	require.NoError(t, err)
+
+	line := "$com $test"
+
+	splitter := splitter.NewCommandSplitterImpl()
+	parser := parser.NewPipeParserImpl(splitter)
+
+	pipe := parser.Parse(line)
+
+	out, err := pipe.RunPipe(nil)
+
+	require.NoError(t, err)
 	require.Equal(t, text, out.Data)
 }
--- internal/pipe/parser/pipe_parser_impl.go
+++ internal/pipe/parser/pipe_parser_impl.go
@@ -1,18 +1,34 @@
 package parser
 
 import (
+	"os"
+	"regexp"
+	"strings"
+
 	"github.com/eqimd/bashgo/internal/command/splitter"
 	"github.com/eqimd/bashgo/internal/pipe"
 )
 
 var _ PipeParser = &PipeParserImpl{}
 
+// Структура для работы парсера, хранит внутри себя CommandSplitter
 type PipeParserImpl struct {
 	splitter splitter.CommandSplitter
 }
 
-// Метод создаёт исполяемый Pipe из строки, описывающей его
+// Метод создаёт исполяемый Pipe из строки, описывающей его, а также подставляет в требуемые места переменные окружения
 func (parser *PipeParserImpl) Parse(s string) *pipe.Pipe {
+	re := regexp.MustCompile(`\$\S*`)
+	for {
+		envWithD := re.FindString(s)
+		envName, _ := strings.CutPrefix(envWithD, "$")
+		if envName == "" {
+			break
+		}
+		envValue := os.Getenv(envName)
+		s = strings.ReplaceAll(s, envWithD, envValue)
+	}
+
 	command, args := parser.splitter.Split(s)
 
 	return pipe.NewPipe(command, args)
